abe99dc110750fc05170f8f336124b20d9d5a223
kalynnka <luhui19970305@hotmail.com>
2025-11-28 11:28:49 +0800

    rename

--- arcanum/__init__.py
+++ arcanum/__init__.py
@@ -1,10 +1,10 @@
 """
-Protocollum - Glue to bind pydantic schemas with various datasources.
+arcanum - Glue to bind pydantic schemas with various datasources.
 
 A Python library designed to seamlessly bind Pydantic schemas with various datasources,
 eliminating the need to manually create templates, factories, and utilities repeatedly.
 """
 
 __version__ = "0.1.0"
-__author__ = "protocollum"
-__email__ = "protocollum@example.com"
+__author__ = "arcanum"
+__email__ = "arcanum@example.com"
