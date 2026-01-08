3428812687a7bd215b26c04ba0167e93cfcc7c07
kalynnka <luhui19970305@hotmail.com>
2026-01-08 15:08:29 +0800

    cleanup

--- arcanus/__init__.py
+++ arcanus/__init__.py
@@ -1,14 +1,3 @@
-"""
-arcanus - Glue to bind pydantic schemas with various datasources.
-
-A Python library designed to seamlessly bind Pydantic schemas with various datasources,
-eliminating the need to manually create templates, factories, and utilities repeatedly.
-"""
-
-__version__ = "0.1.0"
-__author__ = "arcanus"
-__email__ = "arcanus@example.com"
-
 from arcanus.association import (
     Relation,
     RelationCollection,
